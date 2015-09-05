36eb3a2c64ab1d0384186f28fc9369d5e4f106cf
shunobaka <vesselin_minev@abv.bg>
2015-09-05 17:06:31 +0300

    Added unit tests for PlayerScore initialization

---
+++ BullsAndCowsGame.Tests/PlayerScoreTests.cs
@@ -0,0 +1,30 @@
+﻿namespace BullsAndCowsGame.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PlayerScoreTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PlayerScoreShouldThrowArgumentExceptionWhenInitializedWithNullStringForName()
+        {
+            PlayerScore playerScore = new PlayerScore(null, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PlayerScoreShouldThrowArgumentExceptionWhenInitializedWithEmptyStringForName()
+        {
+            PlayerScore playerScore = new PlayerScore(string.Empty, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PlayerScoreShouldThrowArgumentExceptionWhenInitializedWithWhitespaceForName()
+        {
+            PlayerScore playerScore = new PlayerScore("  ", 3);
+        }
+    }
+}
