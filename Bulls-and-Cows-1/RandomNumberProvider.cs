c66a52357dd33a41bdb9b6df71491f2abe06c131
shunobaka <vesselin_minev@abv.bg>
2015-09-05 17:19:13 +0300

    Created RandomNumberProvider class with Singleton pattern implementation

---
+++ Bulls-and-Cows-1/RandomNumberProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace BullsAndCows
+{
+    using System;
+    using BullsAndCows.Contracts;
+
+    public class RandomNumberProvider : IRandomNumberProvider
+    {
+        private static RandomNumberProvider randomNumberProvider;
+
+        private RandomNumberProvider() { }
+
+        public IRandomNumberProvider Instance
+        {
+            get
+            {
+                if (randomNumberProvider == null)
+                {
+                    randomNumberProvider = new RandomNumberProvider();
+                }
+
+                return randomNumberProvider;
+            }
+        }
+
+        public int GenerateNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
