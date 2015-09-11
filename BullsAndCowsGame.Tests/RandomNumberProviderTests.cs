537927853584d7fab134ba7b6d785d92d0aff169
shunobaka <vesselin_minev@abv.bg>
2015-09-11 16:56:56 +0300

    Added unit test checking if the returned number is random for RandomNumberProvider

---
+++ BullsAndCowsGame.Tests/RandomNumberProviderTests.cs
@@ -0,0 +1,20 @@
+﻿namespace BullsAndCowsGame.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BullsAndCows;
+    using BullsAndCows.Contracts;
+
+    [TestClass]
+    public class RandomNumberProviderTests
+    {
+        [TestMethod]
+        public void RandomNumberProviderShouldReturnRandomNumberWhenGenerateNumberAsStringIsCalled()
+        {
+            IRandomNumberProvider randomProvider = RandomNumberProvider.Instance;
+            string firstNumber = randomProvider.GenerateNumberAsString();
+            string secondNumber = randomProvider.GenerateNumberAsString();
+            Assert.AreNotEqual(firstNumber, secondNumber);
+        }
+    }
+}
