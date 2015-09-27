620c04e33c028521e09f0e5fff9102303973c465
ahansb <andrejboyadjiev@gmail.com>
2015-09-28 00:33:45 +0300

    Adding printer NOT READY - to be implemented as simple factory

--- Bulls-and-Cows-1/BullsAndCowsGame.cs
+++ Bulls-and-Cows-1/BullsAndCowsGame.cs
@@ -18,12 +18,15 @@
 
         public static void Play()
         {
-            Console.WriteLine(
-                "Welcome to “Bulls and Cows” game." +
-                "Please try to guess my secret 4-digit number." +
-                "Use 'top' to view the top scoreboard, 'restart'" +
-                "to start a new game and 'help'" +
-                " to cheat and 'exit' to quit the game.");
+            var printer = new Printer();
+            printer.Print(printer.WelcomeMessage);
+            printer.Print(printer.GameRulesMessage);
+            //Console.WriteLine(
+            //    "Welcome to “Bulls and Cows” game." +
+            //    "Please try to guess my secret 4-digit number." +
+            //    "Use 'top' to view the top scoreboard, 'restart'" +
+            //    "to start a new game and 'help'" +
+            //    " to cheat and 'exit' to quit the game.");
 
             Initialize();
             GenerateNumberForGuess();
---
+++ Bulls-and-Cows-1/Printer.cs
@@ -0,0 +1,34 @@
+﻿namespace BullsAndCows
+{
+    using BullsAndCows.Contracts;
+    using System;
+
+    public class Printer : IPrinter
+    {
+        public Printer()
+        {
+        }
+
+
+        public string WelcomeMessage
+        {
+            get
+            {
+                return "Welcome to “Bulls and Cows” game.";
+            }
+        }
+
+        public string GameRulesMessage
+        {
+            get
+            {
+                return @"Please try to guess my secret 4-digit number. Use 'top' to view the top scoreboard, 'restart' to start a new game and 'help' to cheat and 'exit' to quit the game.";
+            }
+        }
+
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
