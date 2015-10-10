cebb02fdc044f942b6030eee438f5f0a133e1917
Andrej Boyadjiev <andrej.boyadjiev@auxcode.com>
2015-10-10 17:19:17 +0300

    Message the one missing

--- BullsAndCowsGame/TwoParameterMessage.cs
+++ BullsAndCowsGame/TwoParameterMessage.cs
@@ -5,7 +5,29 @@ using System.Text;
 
 namespace BullsAndCows
 {
-   public class TwoParameterMessage : Message
+    public class TwoParameterMessage : Message
     {
+
+        private Message message;
+
+        public TwoParameterMessage(Message message, int parameter, int secondParameter)
+        {
+            this.Parameter = parameter;
+            this.SecondParameter = secondParameter;
+            this.Message = message;
+        }
+
+        public int Parameter { get; private set; }
+
+        public object SecondParameter { get; private set; }
+
+        public Message Message { get; private set; }
+        
+
+        public override string Show()
+        {
+            return string.Format(this.Message.Text, this.Parameter,this.SecondParameter);
+        }
+
     }
 }
