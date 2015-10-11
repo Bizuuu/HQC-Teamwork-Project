f74d2c9a3d53fc275c0bab252a981456b87473b8
Andrej Boyadjiev <andrej.boyadjiev@auxcode.com>
2015-10-11 15:05:03 +0300

    comments on unused

--- BullsAndCowsGame/BullsAndCowsGame.cs
+++ BullsAndCowsGame/BullsAndCowsGame.cs
@@ -89,85 +89,85 @@
             return true;
         }
 
-        private void ProcessDigitCommand(string numberAsString)
-        {
-            guessAttemptCounter++;
+        //private void ProcessDigitCommand(string numberAsString)
+        //{
+        //    guessAttemptCounter++;
 
-            if (IsEqualToNumberForGuess(numberAsString))
-            {
-                isGuessed = true;
+        //    if (IsEqualToNumberForGuess(numberAsString))
+        //    {
+        //        isGuessed = true;
 
-                Printer.PrintCongratulationMessage(cheatAttemptCounter, guessAttemptCounter);
-            }
-            else
-            {
-                PrintBullsAndCows(numberAsString);
-            }
-        }
+        //        Printer.PrintCongratulationMessage(cheatAttemptCounter, guessAttemptCounter);
+        //    }
+        //    else
+        //    {
+        //        PrintBullsAndCows(numberAsString);
+        //    }
+        //}
 
-        private void PrintBullsAndCows(string tryNumberString)
-        {
-            int bullsCount = 0;
-            int cowsCount = 0;
+        //private void PrintBullsAndCows(string tryNumberString)
+        //{
+        //    int bullsCount = 0;
+        //    int cowsCount = 0;
 
-            CountBullsAndCows(tryNumberString, ref bullsCount, ref cowsCount);
+        //    CountBullsAndCows(tryNumberString, ref bullsCount, ref cowsCount);
 
-            Printer.PrintMessage(MessageType.WrongNumber, bullsCount, cowsCount);
-        }
+        //    Printer.PrintMessage(MessageType.WrongNumber, bullsCount, cowsCount);
+        //}
 
-        private void CountBullsAndCows(string tryNumberString, ref int bullsCount, ref int cowsCount)
-        {
-            bool[] bulls = new bool[GuessNumberLength];
-            bool[] cows = new bool[AllDigitsCount];
-            bullsCount = CountBulls(tryNumberString, bullsCount, bulls);
-            cowsCount = CountCows(tryNumberString, cowsCount, bulls, cows);
-        }
+        //private void CountBullsAndCows(string tryNumberString, ref int bullsCount, ref int cowsCount)
+        //{
+        //    bool[] bulls = new bool[GuessNumberLength];
+        //    bool[] cows = new bool[AllDigitsCount];
+        //    bullsCount = CountBulls(tryNumberString, bullsCount, bulls);
+        //    cowsCount = CountCows(tryNumberString, cowsCount, bulls, cows);
+        //}
 
-        private int CountCows(string tryNumberString, int cowsCount, bool[] bulls, bool[] cows)
-        {
-            for (int i = 0; i < GuessNumberLength; i++)
-            {
-                int digitForTry = int.Parse(tryNumberString[i].ToString());
-                if (!bulls[i] && !cows[digitForTry])
-                {
-                    cows[digitForTry] = true;
-                    cowsCount = CountCowsForCurrentDigit(tryNumberString, cowsCount, bulls, i);
-                }
-            }
+        //private int CountCows(string tryNumberString, int cowsCount, bool[] bulls, bool[] cows)
+        //{
+        //    for (int i = 0; i < GuessNumberLength; i++)
+        //    {
+        //        int digitForTry = int.Parse(tryNumberString[i].ToString());
+        //        if (!bulls[i] && !cows[digitForTry])
+        //        {
+        //            cows[digitForTry] = true;
+        //            cowsCount = CountCowsForCurrentDigit(tryNumberString, cowsCount, bulls, i);
+        //        }
+        //    }
 
-            return cowsCount;
-        }
+        //    return cowsCount;
+        //}
 
-        private int CountBulls(string tryNumberString, int bullsCount, bool[] bulls)
-        {
-            for (int i = 0; i < GuessNumberLength; i++)
-            {
-                if (tryNumberString[i] == this.NumberForGuess[i])
-                {
-                    bulls[i] = true;
-                    bullsCount++;
-                }
-            }
+        //private int CountBulls(string tryNumberString, int bullsCount, bool[] bulls)
+        //{
+        //    for (int i = 0; i < GuessNumberLength; i++)
+        //    {
+        //        if (tryNumberString[i] == this.NumberForGuess[i])
+        //        {
+        //            bulls[i] = true;
+        //            bullsCount++;
+        //        }
+        //    }
 
-            return bullsCount;
-        }
+        //    return bullsCount;
+        //}
 
-        private int CountCowsForCurrentDigit(string tryNumberString, int cowsCount, bool[] bulls, int i)
-        {
-            for (int j = 0; j < tryNumberString.Length; j++)
-            {
-                if (tryNumberString[i] == this.NumberForGuess[j])
-                {
-                    if (!bulls[j])
-                    {
-                        cowsCount++;
-                        break;
-                    }
-                }
-            }
+        //private int CountCowsForCurrentDigit(string tryNumberString, int cowsCount, bool[] bulls, int i)
+        //{
+        //    for (int j = 0; j < tryNumberString.Length; j++)
+        //    {
+        //        if (tryNumberString[i] == this.NumberForGuess[j])
+        //        {
+        //            if (!bulls[j])
+        //            {
+        //                cowsCount++;
+        //                break;
+        //            }
+        //        }
+        //    }
 
-            return cowsCount;
-        }
+        //    return cowsCount;
+        //}
 
         private bool IsEqualToNumberForGuess(string tryNumber)
         {
@@ -179,51 +179,51 @@
             return false;
         }
 
-        private void ProcessTextCommand(string command)
-        {
-            switch (command.ToLower())
-            {
-                case "top":
-                    Printer.PrintLeaderBoard(ScoreBoard.LeaderBoard);
-                    break;
-                case "help":
-                    RevealDigit();
-                    cheatAttemptCounter++;
-                    break;
-                case "restart":
-                    CreateNewGame();
-                    return;
-                case "exit":
-                    Printer.PrintMessage(MessageType.Exit);
-                    Environment.Exit(1);
-                    break;
-                default:
-                    Printer.PrintMessage(MessageType.InvalidCommand);
-                    break;
-            }
-        }
+        //private void ProcessTextCommand(string command)
+        //{
+        //    switch (command.ToLower())
+        //    {
+        //        case "top":
+        //            Printer.PrintLeaderBoard(ScoreBoard.LeaderBoard);
+        //            break;
+        //        case "help":
+        //            RevealDigit();
+        //            cheatAttemptCounter++;
+        //            break;
+        //        case "restart":
+        //            CreateNewGame();
+        //            return;
+        //        case "exit":
+        //            Printer.PrintMessage(MessageType.Exit);
+        //            Environment.Exit(1);
+        //            break;
+        //        default:
+        //            Printer.PrintMessage(MessageType.InvalidCommand);
+        //            break;
+        //    }
+        //}
 
-        private void RevealDigit()
-        {
-            bool flag = false;
-            int c = 0;
-            while (!flag &&
-                   c != 2 * this.NumberForGuess.Length)
-            {
-                int digitForReveal = RandomNumberProvider.GenerateNumber(0, 4);
+        //private void RevealDigit()
+        //{
+        //    bool flag = false;
+        //    int c = 0;
+        //    while (!flag &&
+        //           c != 2 * this.NumberForGuess.Length)
+        //    {
+        //        int digitForReveal = RandomNumberProvider.GenerateNumber(0, 4);
 
-                if (helpingNumber[digitForReveal] == 'X')
-                {
-                    helpingNumber[digitForReveal] =
-                    this.NumberForGuess[digitForReveal];
-                    flag = true;
-                }
+        //        if (helpingNumber[digitForReveal] == 'X')
+        //        {
+        //            helpingNumber[digitForReveal] =
+        //            this.NumberForGuess[digitForReveal];
+        //            flag = true;
+        //        }
 
-                c++;
-            }
+        //        c++;
+        //    }
 
-            Printer.PrintHelpingNumber(helpingNumber);
-        }
+        //    Printer.PrintHelpingNumber(helpingNumber);
+        //}
 
         private void CreateNewGame()
         {
