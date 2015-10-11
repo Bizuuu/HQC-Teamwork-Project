704e1600fb7b648d093c7165e3bd8743ed0d017f
shunobaka <vesselin_minev@abv.bg>
2015-10-11 17:57:03 +0300

    Documented the BullsAndCowsGame, Arcade and Counter

--- BullsAndCowsGame/BullsAndCowsArcade.cs
+++ BullsAndCowsGame/BullsAndCowsArcade.cs
@@ -4,15 +4,27 @@
     using Contracts;
     using Messages;
 
+    /// <summary>
+    /// Class implementing the Facade design pattern used for creating all needed objects for the game and starting the game.
+    /// </summary>
     public class BullsAndCowsArcade
     {
+        /// <summary>
+        /// A constant containing the amount of players which will be displayed in the leaderboard.
+        /// </summary>
         private const int MaxPlayersInScoreboard = 5;
 
+        /// <summary>
+        /// Public method used by the user to start the process of starting the game.
+        /// </summary>
         public void StartGame()
         {
             this.Initialize();
         }
 
+        /// <summary>
+        /// Creates the needed requirements for the game, initializes it and starts it.
+        /// </summary>
         private void Initialize()
         {
             NumberCommandProcessor numberCommandProcessor = new NumberCommandProcessor();
--- BullsAndCowsGame/BullsAndCowsCounter.cs
+++ BullsAndCowsGame/BullsAndCowsCounter.cs
@@ -2,13 +2,35 @@
 {
     using System;
 
+    /// <summary>
+    /// Used for counting the bulls and cows.
+    /// </summary>
     public class BullsAndCowsCounter : IDisposable
     {
+        /// <summary>
+        /// The length of the number which has to be guessed.
+        /// </summary>
         private const byte GuessNumberLength = 4;
+
+        /// <summary>
+        /// The different digits which can be used to form a number.
+        /// </summary>
         private const byte AllDigitsCount = 10;
+
+        /// <summary>
+        /// A helper used to count the number of bulls.
+        /// </summary>
         private bool[] areBulls;
+
+        /// <summary>
+        /// A helper used to count the number of cows.
+        /// </summary>
         private bool[] areCows;
 
+        /// <summary>
+        /// Initializes a new instance of the BullsAndCowsCounter class.
+        /// </summary>
+        /// <param name="numberForGuess">The number which needs to be guessed.</param>
         public BullsAndCowsCounter(string numberForGuess)
         {
             this.NumberForGuess = numberForGuess;
@@ -16,14 +38,25 @@
             this.areCows = new bool[AllDigitsCount];
         }
 
+        /// <summary>
+        /// Gets or sets the number which has to be guessed.
+        /// </summary>
         private string NumberForGuess { get; set; }
 
+        /// <summary>
+        /// Disposes the helpers used for calculating the bulls and cows.
+        /// </summary>
         public void Dispose()
         {
             this.areBulls = new bool[GuessNumberLength];
             this.areCows = new bool[AllDigitsCount];
         }
 
+        /// <summary>
+        /// Public method used to initiate counting of the bulls and cows.
+        /// </summary>
+        /// <param name="suggestedNumber">The number with which the user tried.</param>
+        /// <returns>Returns the bulls and cows in a BullsAndCowsResult.</returns>
         public BullsAndCowsResult CountBullsAndCows(string suggestedNumber)
         {
             var bulls = this.CountBulls(suggestedNumber);
@@ -31,6 +64,11 @@
             return new BullsAndCowsResult(bulls, cows);
         }
 
+        /// <summary>
+        /// Counts the number of bulls.
+        /// </summary>
+        /// <param name="suggestedNumber">The guessed number by the user.</param>
+        /// <returns>Returns an integer with the number of bulls.</returns>
         private int CountBulls(string suggestedNumber)
         {
             int bullsCount = 0;
@@ -46,6 +84,11 @@
             return bullsCount;
         }
 
+        /// <summary>
+        /// Counts the number of cows.
+        /// </summary>
+        /// <param name="suggestedNumber">The guessed by the user number.</param>
+        /// <returns>Returns the number of cows.</returns>
         private int CountCows(string suggestedNumber)
         {
             int cowsCount = 0;
